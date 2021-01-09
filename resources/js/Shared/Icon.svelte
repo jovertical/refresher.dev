63eaeae0049a8741dbec53985180b23a852ee611
Jovert Palonpon <jovertical@gmail.com>
2021-01-09 16:31:09 +0800

    Delete search component

--- resources/js/Shared/Icon.svelte
+++ resources/js/Shared/Icon.svelte
@@ -60,13 +60,6 @@
             stroke-width="2"
             d="M4 6h16M4 12h16M4 18h16"
         ></path>
-    {:else if name === 'search'}
-        <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-        ></path>
     {:else if name === 'user-circle'}
         <path
             stroke-linecap="round"
